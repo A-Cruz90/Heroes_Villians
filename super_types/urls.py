b6ddcd37371feb2dd01b77468254ad79326fc792
Angel Cruz <cruzangel99@gmail.com>
2022-12-02 21:12:00 -0500

    I believe i have reached the endpoint, but since i have no list nothing is comming back. Testing a view more lines before completion

--- super_types/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('127.0.0.1:8000/api/super_types/') ,
-    path('127.0.0.1:8000/api/super_types/<int:pk>/')
-]
--- supers/urls.py
+++ supers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('127.0.0.1:8000/api/supers/',)
-    path('127.0.0.1:8000/api/supers/<int:pk>/')
+    path('127.0.0.1:8000/api/supers/', views.super_list),
+    path('127.0.0.1:8000/api/supers/<int:pk>/',views.supers_detail ),
+
 ]
